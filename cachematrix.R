d37500939e6a703583be94678e4874cce675113d
nathal <nathalieh@google.com>
2015-08-20 21:34:48 +0200

    Revert "Delete cachematrix.R"
    This reverts commit b795820d09e7ba8bb19fb2b0cd28f6476c74196b.

---
+++ cachematrix.R
@@ -0,0 +1,15 @@
+## Put comments here that give an overall description of what your
+## functions do
+## this is a new version
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
